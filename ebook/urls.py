46407c95b3c93454a3cad986ec3a56173459ecda
Asir Mosaddek Sakib <sakib.rangpur@gmail.com>
2018-08-15 00:02:20 +0600

    Added more views

--- ebook/urls.py
+++ ebook/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('categories/', views.categories),
+    path('category/<int:category_id>', views.category),
     path('ebooks/', views.ebooks),
+    path('ebook/<int:ebook_id>', views.ebook)
 ]
--- ebook/views.py
+++ ebook/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Ebook
+
 def home(request):
 	pass
 
@@ -10,8 +11,22 @@ def categories(request):
 		'categories': categories,
 		})
 
+def category(request, category_id):
+	category = get_object_or_404(Category, pk = category_id)
+	ebooks = category.ebook_set.all()
+	return render(request, 'ebook/category.html', {
+		'category' : category,
+		'ebooks' : ebooks
+		})
+
 def ebooks(request):
 	ebooks = Ebook.objects.all()
 	return render(request, 'ebook/ebooks.html', {
 		'ebooks': ebooks,
+		})
+
+def ebook(request, ebook_id):
+	ebook = get_object_or_404(Ebook, pk = ebook_id)
+	return render(request, 'ebook/ebook.html', {
+		'ebook' : ebook
 		})
