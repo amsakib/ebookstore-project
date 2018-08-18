f2826cd02c03b220eedeb6a29c1ddee62e9fd6b7
Asir Mosaddek Sakib <sakib.rangpur@gmail.com>
2018-08-18 19:04:36 +0600

    Added pagination

--- ebook/views.py
+++ ebook/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Ebook
+from django.core.paginator import Paginator
 
 def home(request):
     ebooks = Ebook.objects.order_by('-id')[:8]
@@ -16,6 +17,14 @@ def categories(request):
 def category(request, category_id):
     category = get_object_or_404(Category, pk = category_id)
     ebooks = category.ebook_set.all()
+
+    paginator = Paginator(ebooks, 12)
+    try:
+        page = request.GET['page']
+    except:
+        page = 1
+    ebooks = paginator.get_page(page)
+
     return render(request, 'ebook/category.html', {
 		'category' : category,
 		'ebooks' : ebooks
@@ -23,6 +32,12 @@ def category(request, category_id):
 
 def ebooks(request):
     ebooks = Ebook.objects.all()
+    paginator = Paginator(ebooks, 12)
+    try:
+        page = request.GET['page']
+    except:
+        page = 1
+    ebooks = paginator.get_page(page)
     return render(request, 'ebook/ebooks.html', {
 		'ebooks': ebooks,
 		})
